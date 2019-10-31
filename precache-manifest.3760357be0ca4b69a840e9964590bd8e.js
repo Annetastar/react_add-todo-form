3a4cc4864a960cdefc65ab968328f0a94d92fdc0
Anna Kazantseva <annetastara@gmail.com>
2019-10-31 15:02:41 +0200

    Updates

---
+++ precache-manifest.3760357be0ca4b69a840e9964590bd8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dfad4ba2e7a1754fc5225718702211",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "44470ee112b1d267deb4",
+    "url": "/react_add-todo-form/static/css/main.d79f2877.chunk.css"
+  },
+  {
+    "revision": "6b1643da7efe502c3ecc",
+    "url": "/react_add-todo-form/static/js/2.3eab389f.chunk.js"
+  },
+  {
+    "revision": "44470ee112b1d267deb4",
+    "url": "/react_add-todo-form/static/js/main.e3297c66.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  },
+  {
+    "revision": "96d557b977c8ac65bf789bfb932199f5",
+    "url": "/react_add-todo-form/static/media/bg.96d557b9.jpg"
+  }
+]);
